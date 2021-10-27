0eaeb024702979d076cac8acdb411e968b39771d
angelvalenciamorales <21340736@itnogales.edu.mx>
2021-10-27 13:31:15 +0200

    Esta es una practica de la univesidad

---
+++ edadesForm/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace edadesForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.Registro = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.dateEdad = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblreseptor2 = new System.Windows.Forms.Label();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.Transparent;
+            this.iconButton1.Enabled = false;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.UserShield;
+            this.iconButton1.IconColor = System.Drawing.Color.White;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 200;
+            this.iconButton1.Location = new System.Drawing.Point(26, 125);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(263, 336);
+            this.iconButton1.TabIndex = 0;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // Registro
+            // 
+            this.Registro.AutoSize = true;
+            this.Registro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registro.Location = new System.Drawing.Point(80, 362);
+            this.Registro.Name = "Registro";
+            this.Registro.Size = new System.Drawing.Size(124, 31);
+            this.Registro.TabIndex = 1;
+            this.Registro.Text = "Registro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(408, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe MDL2 Assets", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(408, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe MDL2 Assets", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(408, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha de nacimiento";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(674, 106);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(223, 31);
+            this.txtNombres.TabIndex = 5;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(674, 242);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(223, 31);
+            this.txtApellidos.TabIndex = 6;
+            // 
+            // dateEdad
+            // 
+            this.dateEdad.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateEdad.Location = new System.Drawing.Point(674, 362);
+            this.dateEdad.Name = "dateEdad";
+            this.dateEdad.Size = new System.Drawing.Size(223, 31);
+            this.dateEdad.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txtedad);
+            this.panel1.Controls.Add(this.txtn);
+            this.panel1.Controls.Add(this.lblreseptor2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(947, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 736);
+            this.panel1.TabIndex = 11;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Location = new System.Drawing.Point(26, 481);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(246, 59);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Registrar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(26, 588);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 59);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblreseptor2
+            // 
+            this.lblreseptor2.AutoSize = true;
+            this.lblreseptor2.BackColor = System.Drawing.Color.Transparent;
+            this.lblreseptor2.Location = new System.Drawing.Point(49, 96);
+            this.lblreseptor2.Name = "lblreseptor2";
+            this.lblreseptor2.Size = new System.Drawing.Size(0, 25);
+            this.lblreseptor2.TabIndex = 14;
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(56, 104);
+            this.txtn.Multiline = true;
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(309, 61);
+            this.txtn.TabIndex = 15;
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(56, 240);
+            this.txtedad.Multiline = true;
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(309, 61);
+            this.txtedad.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1387, 736);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateEdad);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Registro);
+            this.Controls.Add(this.iconButton1);
+            this.Name = "Form1";
+            this.Text = "Limpiar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.Label Registro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.DateTimePicker dateEdad;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblreseptor2;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.TextBox txtn;
+    }
+}
+
---
+++ edadesForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace edadesForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //proceso de variable
+            String nombres, apellido;
+            nombres = txtNombres.Text.ToUpper();
+            apellido = txtApellidos.Text.ToUpper();
+
+            int edad = DateTime.Now.Year - dateEdad.Value.Year;
+
+            //agregamos edad..
+            DateTime nacimientoAhora = dateEdad.Value.AddYears(edad);
+
+            //Condicional la cual decrementa en 1...
+            if (DateTime.Now.CompareTo(nacimientoAhora) > 0)
+            {
+                edad = edad - 1 + 1;
+
+            }
+
+            else
+            {
+                edad = edad - 1;
+            }
+
+            txtedad.Text = Convert.ToString("La edad es:" + edad);
+
+            txtn.Text = Convert.ToString("El nombre completo es: " + nombres + " " + apellido + " ");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            txtn.Text = " ";
+            txtedad.Text = " ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
